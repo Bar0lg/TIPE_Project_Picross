f4248904ecfa6e9a07f05b7e183d102e9f652dbf
Bar0lg <barolgjeu@gmail.com>
2024-06-09 19:16:46 +0200

    PRESQUE_FINI

--- spe/gather_data.py
+++ spe/gather_data.py
@@ -3,7 +3,7 @@ import matplotlib
 import numpy as np
 import sys
 
-font = {'size' : 18}
+font = {'size' : 15}
 
 matplotlib.rc('font', **font)
 
@@ -49,10 +49,10 @@ def g_data(filename):
 def main_2():
     all_backtrack = [g_data(x) for x in backtrack_f]
     all_backtrack_nd = [g_data(x) for x in backtrack_nd_f]
-    valid_means =       np.asarray([np.mean(x["valid_t"]) for x in all_backtrack])
-    valid_means_nd =    np.asarray([np.mean(x["valid_t"]) for x in all_backtrack_nd])
-    algos_mean =        np.asarray([np.mean(x["algo_t"]) for x in all_backtrack])
-    algos_mean_nd =     np.asarray([np.mean(x["algo_t"]) for x in all_backtrack_nd])
+    valid_means =       np.asarray([np.median(x["valid_t"]) for x in all_backtrack])
+    valid_means_nd =    np.asarray([np.median(x["valid_t"]) for x in all_backtrack_nd])
+    algos_mean =        np.asarray([np.median(x["algo_t"]) for x in all_backtrack])
+    algos_mean_nd =     np.asarray([np.median(x["algo_t"]) for x in all_backtrack_nd])
     n =list(map(str, range(4,22)))
     valid_means,valid_means_nd = equilibrate(valid_means,valid_means_nd)
     algos_mean,algos_mean_nd = equilibrate(algos_mean,algos_mean_nd)
@@ -68,10 +68,10 @@ def main_2():
 def main():
     all_backtrack = [g_data(x) for x in backtrack_f]
     all_backtrack_nd = [g_data(x) for x in backtrack_nd_f]
-    valid_means =       np.asarray([np.median(x["valid_t"]) for x in all_backtrack])
-    valid_means_nd =    np.asarray([np.median(x["valid_t"]) for x in all_backtrack_nd])
-    algos_mean =        np.asarray([np.median(x["algo_t"]) for x in all_backtrack])
-    algos_mean_nd =     np.asarray([np.median(x["algo_t"]) for x in all_backtrack_nd])
+    valid_means =       np.asarray([np.mean(x["valid_t"]) for x in all_backtrack])
+    valid_means_nd =    np.asarray([np.mean(x["valid_t"]) for x in all_backtrack_nd])
+    algos_mean =        np.asarray([np.mean(x["algo_t"]) for x in all_backtrack])
+    algos_mean_nd =     np.asarray([np.mean(x["algo_t"]) for x in all_backtrack_nd])
     n =list(map(str, range(4,22)))
     x = np.arange(len(n))
     valid_means,valid_means_nd = equilibrate(valid_means,valid_means_nd)
@@ -87,7 +87,7 @@ def main():
     plt.bar(x + offset,valid_means_nd,width,label="Création des automates non-déterministes",color="black")
     plt.bar(x+offset,algos_mean_nd,width,label="Algorithme de backtracking avec automates déterministes",bottom=valid_means_nd,color="blue")
     plt.yscale("log")
-    plt.title("Médiannes du temps d'exécution des algorithmes de backtracking")
+    plt.title("Moyennes du temps d'exécution des algorithmes de backtracking")
     plt.ylabel("Secondes")
     plt.xlabel("Taille du graphe")
     
@@ -97,36 +97,37 @@ def main():
 
 
 def chance_g():
+    size= 12
     all_chance = [g_data(x) for x in chance12_f]
     solved = np.asarray([np.mean(x["nb_lr"]) for x in all_chance])
     n = list(map(str,range(10,100,10)))
     plt.cla()
     plt.bar(n,solved,width=0.50,label="Cases trouvées")
     plt.legend(loc='upper left')
-    plt.axhline(12*12,linestyle="--")
-    plt.ylim(0,12*12+5)
+    plt.axhline(size*size,linestyle="--")
+    plt.ylim(0,size*size+5)
     plt.ylabel("Cases trouvées")
     plt.xlabel("Probabilité qu'une case soit noire (en %)")
-    plt.title("Moyennes des cases trouvées par les règles logiques dans une grille de taille 12")
+    plt.title("Moyennes des cases trouvées par les règles logiques dans une grille de taille " + str(size))
     plt.show()
 
 def comprar():
-    all_data = [g_data(x) for x in backtrack_f]
-    all_data_lr = [g_data(x) for x in backtrack_nd_f]
+    all_data = [g_data(x) for x in backtrack_lr_f]
+    all_data_lr = [g_data(x) for x in backtrack_nd_lr_f]
 
     time_data =         np.asarray([np.median(x["total_t"]) for x in all_data])
     time_data_lr =      np.asarray([np.median(x["total_t"]) for x in all_data_lr])
     time_data,time_data_lr = equilibrate(time_data,time_data_lr)
-    n =list(map(str, range(4,22)))
+    n =list(map(str, range(4,21)))
     x = np.arange(len(n))
     width = 0.40
     offset = width
     plt.cla()
-    plt.bar(x+offset,time_data,width,label="Backtracking deterministe avec regles logiques",color="blue")
+    plt.bar(x+offset,time_data,width,label="Backtracking avec automates déterministes et règles logiques",color="blue")
     offset += offset
-    plt.bar(x+offset,time_data_lr,width,label="Backtracking non deterministe avec regles logiques",color="orange")
+    plt.bar(x+offset,time_data_lr,width,label="Backtracking avec automates non-déterministes et règles logiques",color="orangered")
     plt.yscale("log")
-    plt.title("Comparaison du temps d'execution (médianes)")
+    plt.title("Comparaison du temps d'exécution (médianes)")
     plt.ylabel("Secondes")
     plt.xlabel("Taille du graphe")
     
@@ -153,7 +154,7 @@ backtrack_f = [
 "../DATA/backtrack_500_18.txt",
 "../DATA/backtrack_100_19.txt",
 "../DATA/backtrack_100_20.txt",
-"../DATA/backtrack_100_21.txt",
+"../DATA/backtrack_100_21.txt"
 ]
 
 backtrack_lr_f = [
